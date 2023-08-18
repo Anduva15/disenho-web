c8032ba85123c6157686ed3bbfcc0149a7ac1507
anduva15 <aullova25@gmail.com>
2023-08-18 13:40:25 -0600

    login endpoint added

---
+++ src/routes/login.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import login from '../controllers/login.js';
+
+const router = Router();
+const baseUrl = '/login';
+
+
+//router.post(baseUrl, login.create);
+
+export default router;
