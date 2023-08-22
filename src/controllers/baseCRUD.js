44785320f9a787f19f6aee18517a6a9afc0946f0
Nicolas_BL <nicolasbeisswenger@hotmail.com>
2023-08-22 15:01:37 -0600

    Actualización de errores en el baseCRUD

--- src/controllers/baseCRUD.js
+++ src/controllers/baseCRUD.js
@@ -1,8 +1,8 @@
 export default (schemaName, Schema) => {
   const handleSuccess = (res, message) => res.status(200).json(message);
-  const handleFailure = (res, message) => res.status(500).json(message);
+  //const handleFailure = (res, message) => res.status(500).json(message);
 
-
+  //app.use()
 
   return {
     getAll: async (req, res) => {
@@ -12,95 +12,109 @@ export default (schemaName, Schema) => {
             req.query[key] ? { ...acc, [key]: req.query[key] } : acc,
           {}
         );
-
         const items = await Schema.find(findParams);
-        if (schemaName === "users")
-        {
-          const itemsWithoutPassword = items.map(item => 
-          {
-            const {password, ...itemWithoutPassword } = item.toObject();
-            return itemWithoutPassword;
-          });
-          handleSuccess(res, itemsWithoutPassword);
-        } else {
-          handleSuccess(res, items);
-        }
-      } catch {
-        handleFailure(res, {
-          error: `Error obteniendo todo los ${schemaName}`,                         
-        });
-        
-        console.error('Error en el proceso de obtención');
+        handleSuccess(res, items);
+      } 
+      catch {
+        //handleFailure(res, {error: `Error obteniendo todo los ${schemaName}`});*/
+        res.status(500).json({ error: 'Invalid credentials' });
       }
     },
+
+
+
     get: async (req, res) => {
-      try 
-      {
+      try {
         const id = req.params.id;
         const findParams = isNaN(id) ? { _id: id } : { id };
         const item = await Schema.findOne(findParams);
-        
-        if(schemaName === "users"){
-          const { password, ...itemWithoutPassword } = item.toObject();
-          handleSuccess(res, itemWithoutPassword);
-        } else {
-          handleSuccess(res, item);
-        }
-      } catch {
-        handleFailure(res, {
-          error: `Error: El ${_id} no existe en ${schemaName}`,
-        });
 
-        console.error('Usuario no encontrado');
+        handleSuccess(res, item);
+      } catch {
+        res.status(500).json({error: 'Failed process' });
+        console.error('Proceso fallido');
       }
     },
+
+
+
     create: (req, res) => {
       const item = req.body;
       const newItem = new Schema(item);
-     
-      newItem.save(e => {
-        if (e) {
-          console.error('Error en la creación del usuario');
-          console.log(e)
-          handleFailure(res, { error: `'Error creando ${schemaName}'` });
-        } else {
-          handleSuccess(res, newItem);
-        }
-      });
+
+      try{
+        newItem.save(e => {
+          if (e) {
+            console.error('Error en la creación del usuario');
+            res.status(500).json({ error: `'Error creando ${schemaName}'` });
+          } else {
+            handleSuccess(res, newItem);
+          }
+        });
+      }
+      catch{
+        res.status(500).json({error: 'Failed process' });
+        console.error('Proceso fallido');
+      }
+
+      
     },
+    
+    
+    
     update: async (req, res) => {
       const _id = req.params.id;
+      const bodyId = req.body._id;
+      
       try {
         const item = await Schema.findOne({ _id });
 
-        // add 404
-        // validate if para with body id
+        if (bodyId && bodyId !== _id) {// validate if para with body id
+          return res.status(400).json({ error: 'usuario no valido' });
+        }
 
         Object.keys(req.body).forEach(key => {
           item[key] = req.body[key];
         });
 
         await item.save();
-
         handleSuccess(res, item);
-      } catch {
-        handleSuccess(res, { error: `${schemaName} con id ${_id}, no existe` 
-        });
-
-        console.error('Error durante el proceso de carga');
+        res.status(200).json({message: 'Proceso exitso' });
+      } 
+      catch {
+        //handleSuccess(res, { error: `${schemaName} con id ${_id}, no existe`});
+        res.status(404).json({error: 'Invalid credentials' });// add 404
       }
     },
+
+
+
     delete: async (req, res) => {
       const _id = req.params.id;
+      const bodyId = req.body._id;
 
-      try {
-        await Schema.deleteOne({ _id });
-
-        handleSuccess(res, { success: 'Exito al borrar' });
-      } catch {
-        handleSuccess(res, { error: `${schemaName} con id ${_id}, no existe` });
-        console.error('Error al eliminar el usuario');
+      try{
+        if (bodyId && bodyId !== _id) {// validate if para with body id
+          return res.status(400).json({ error: 'usuario no valido' });
+        }
+  
+        try {
+          await Schema.deleteOne({ _id });
+          res.status(200).json({message: '' });
+          handleSuccess(res, { success: 'Exito al borrar' });
+        } 
+        catch {
+          handleSuccess(res, { error: `${schemaName} con id ${_id}, no existe` });
+          console.error('Error al eliminar el usuario');
+          return res.status(400).json({ error: 'usuario no valido' });
+        }
       }
+      catch{
+        console.error('Error en el proceso');
+        return res.status(500).json({ error: 'Error en el proceso' });
+      }
+
+      
     },
   };
 };
